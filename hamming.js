057caa4107bfd5ee0d1991cebb8ea4670293b61d
Kareha Agesa <kmaagesa@gmail.com>
2020-12-13 15:04:09 -0800

    all tests passing

--- hamming.js
+++ hamming.js
@@ -3,6 +3,23 @@
 // convenience to get you started writing code faster.
 //
 
-export const compute = () => {
-  throw new Error("Remove this statement and implement this function");
+export const compute = (string1, string2) => {
+  if (string1.length === 0 && string2.length !== 0){
+    throw new Error('left strand must not be empty');
+  }
+  if (string1.length !== 0 && string2.length === 0){
+    throw new Error('right strand must not be empty');
+  }
+  if (string1.length !== string2.length){
+  throw new Error('left and right strands must be of equal length');
+  }
+  
+  let differences = 0;
+  // randomly picked length of 1st string
+  for (let i = 0; i <= string1.length; i += 1){
+    if (string1[i] !== string2[i]) {
+      differences += 1;
+    }
+  }
+  return differences;
 };
